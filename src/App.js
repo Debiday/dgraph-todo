1bd5a59618aee2d08daa27d9732bcd912090ae18
Deborah Ong <deborah.oyt@gmail.com>
2021-10-21 12:23:58 -0700

    add hooks and docker yml

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css'
+import React from 'react'
+import { HashRouter, Route } from "react-router-dom";
+import 'todomvc-app-css/index.css'
+
+import Footer from "./components/Footer";
+import TodoList from './containers/TodoList';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <HashRouter>
+        <React.Fragment>
+        <div className="todoapp">
+          <Route path="/:filter?" component={TodoList} />
+        </div>
+        <Footer />
+        </React.Fragment>
+      </HashRouter>
     </div>
-  );
+  )
 }
 
 export default App;
